87194c68c36bde3e3492f73c7cf31b1df46e60db
poy <Yann.Poltera@meteoswiss.ch>
2016-03-10 15:00:53 +0100

    analysis kse

--- read_overlap_cor_v3_kse.m
+++ read_overlap_cor_v3_kse.m
@@ -316,6 +316,7 @@ p = polyfit(A,B,1);
 SStot = sum((B'-repmat(mean(B),length(B),1)).^2);
 SSres = sum((B'-polyval(p,A')).^2);
 R2 = 1-SSres/SStot;
+R2adjusted = 1-SSres/SStot*(length(A)-1)/(length(A)-2);
 RMSE = sqrt(SSres/length(A));
 RMSErel = 100*RMSE / sqrt(sum(polyval(p,A').^2)/length(A));
 % disp(['Rsquared of the fit: ' num2str(R2)])
@@ -456,6 +457,16 @@ for j=1:find(range<=1200,1,'last')
     c(j) = fo.p2(1);
     r2(j) = gof.rsquare;
     rmse(j) = gof.rmse;
+    
+%     mdl = fitlm(x_fit,y_fit,'linear');
+% %     mdl = fitlm(x_fit,y_fit,'linear','RobustOpts','bisquare';
+% %     mdl = fitlm(x_fit,y_fit,'linear','RobustOpts','bisquare','Weights',weights_fit);
+%     a(j) = 0;
+%     b(j) = mdl.Coefficients(1).Estimate;
+%     c(j) = mdl.Coefficients(2).Estimate;
+%     r2(j) = mdl.Rsquared.Adjusted;
+%     rmse(j) = mdl.RMSE;
+    
     
     %         scatter(x_fit,y_fit,'filled');grid on;box on;line([0 40],feval(fo,[0 40]),'color','k','linewidth',2);
     %         xlim([0 40]);xlabel('T [°C]');
